501153acb0d032f82d7a39d748b7e750536b69c3
César Acual <augustoacual@gmail.com>
2022-11-09 22:58:33 -0600

    Crear funcionalidad para crear estados

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -9,14 +9,20 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStatusTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function an_authenticated_user_can_create_statuses()
     {
-        $this->actingAs(factory(User::class)->create());
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
 
         $this->post(route('statuses.store'), ['body' => 'Mi primer post']);
 
         $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
             'body' => 'Mi primer post'
         ]);
     }
