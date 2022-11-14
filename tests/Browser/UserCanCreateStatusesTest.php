a804b3b793735d2eadc0fd9b25a3081920bc3069
César Acual <augustoacual@gmail.com>
2022-11-14 11:28:16 -0600

    Probar crear un status utilizando Laravel Dusk

---
+++ tests/Browser/UserCanCreateStatusesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UserCanCreateStatusesTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function user_can_create_statuses()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit('/')
+                ->type('body', 'Mi primer status')
+                ->press('#create-status')
+                ->assertSee('Mi primer status');
+        });
+    }
+}
