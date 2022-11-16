96c0992aa5080f77589957ea245efd49346f49f4
César Acual <augustoacual@gmail.com>
2022-11-16 17:59:35 -0600

    Deseniando el formulario de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
-<form action="{{ route('statuses.store') }}" method="POST">
-    @csrf
-    <textarea name="body" cols="30" rows="10"></textarea>
-    <button type="submit" id="create-status">Publicar estado</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+<div class="container"></div>
+    <div class="row">
+        <div class="col-8 mx-auto">
+            <div class="card border-0 bg-light">
+                <form action="{{ route('statuses.store') }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <textarea name="body" class="form-control border-0 bg-light" placeholder="¿Qué estás pensando César?"></textarea>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" id="create-status" class="btn btn-primary">Publicar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
