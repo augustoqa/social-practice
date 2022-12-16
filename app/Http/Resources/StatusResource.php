60150530060fede2ee6b5ba60e04e277b1cd86b2
Cesar Acual <agustoacual@gmail.com>
2022-12-16 06:47:53 -0600

    Testeando Recurso API de Status

---
+++ app/Http/Resources/StatusResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StatusResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'body' => $this->resource->body,
+            'user_name' => $this->user->name,
+            'user_avatar' => 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+            'ago' => $this->created_at->diffForHumans(),
+        ];
+    }
+}
